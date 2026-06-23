e8ef45a0b4cc35c392f1ae255ab286dabfcbec53
Gabrielle <ogdd3101@gmail.com>
2026-06-23 15:10:48 +0100

    modified dashboard layout

--- src/features/dashboard/pages/DashboardPage.jsx
+++ src/features/dashboard/pages/DashboardPage.jsx
@@ -4,6 +4,9 @@ import PageContainer from "../../../components/layout/PageContainer";
 import GreetingSection from "../components/GreetingSection";
 import AiLauncher from "../components/AiLauncher";
 import RecentBoardsSection from "../components/RecentBoardsSection";
+import HiveActivityPanel from "../components/HiveActivityPanel";
+
+import '../styles/dashboard-layout.css'
 
 const user = {
     name: "Gabrielle",
@@ -15,13 +18,10 @@ function DashboardPage() {
             <PageContainer>
                 <GreetingSection user={user} />
                 <AiLauncher />
-                <div
-                    style={{
-                        marginTop: "48px",
-                    }}
-                >
-                    <RecentBoardsSection />
-                </div>
+                <div className="dashboard-grid">
+                    <div><RecentBoardsSection /></div>
+                    <HiveActivityPanel />
+                </div>                
             </PageContainer>
         </AppShell>
     );
